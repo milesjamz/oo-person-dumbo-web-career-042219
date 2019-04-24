8c38bb071c1f6247f11d070a0d6545b8c9be5732
Miles Marillo <mjm03@hampshire.edu>
2019-04-24 19:29:31 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -33,15 +33,15 @@ end
 #     @hygiene  < 0 
 #   end 
   
-#   def happiness=()
-#   if @happiness > 10
-#     happiness == 10
-#   elsif @happiness < 0
-#     happiness == 0
-#   else
-#     return @happiness
-#   end
-# end
+  def happiness=()
+  if @happiness > 10
+    happiness == 10
+  elsif @happiness < 0
+    happiness == 0
+  else
+    return @happiness
+  end
+end
 
   def get_paid(salary)
   @bank_account = (@bank_account + salary)
