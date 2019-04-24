3a7eaca4aa617d9f7e1da6c558dcb574d6344bc2
Miles Marillo <mjm03@hampshire.edu>
2019-04-24 19:27:08 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,9 +2,8 @@ require 'pry'
 
 class Person
   
-attr_accessor  :bank_account 
-attr_reader  :name, :happiness, :hygiene
-attr_writer  
+attr_accessor  :bank_account, :happiness, :hygiene
+attr_reader  :name
   
   def initialize(name)
   @name = name
